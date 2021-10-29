2aa27ea20ee0612ed55306845f8bd0f697638d09
DariaRusovich <rusovichdasha41@gmail.com>
2021-10-29 15:24:29 +0300

    add-api

--- src/api/api.js
+++ src/api/api.js
@@ -2,27 +2,27 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:1234',
-  headers: { 'Content-type': 'application/json;charset=utf-8' }
+  headers: { 'Content-type': 'application/json;charset=utf-8' },
 });
 
+api.interceptors.response.use(
+  (response) => {
+    return [response.data, null];
+  },
+  (error) => {
+    return [null, error];
+  }
+);
 
-api.interceptors.response.use((response) => {
-  return [response.data, null]
-}, (error) => {
-  return [null, error]
-});
-
-export function getCats() {
-  return api.get('/categories')
-}
 export function getSnippets() {
-  return api.get('/snippets')
+  return api.get('/snippets');
+}
+export function getSnippet(id) {
+  return api.get(`/snippets/${id}`);
 }
 export function createSnippet(snippet) {
-  return api.post(`/snippets`, snippet)
+  return api.post(`/snippets`, snippet);
 }
-export function createCat(cat) {
-  console.log(cat);
-  return api.post('/categories', cat)
-  
+export function updateSnippet(id, snippet) {
+  return api.patch(`/snippets/${id}`, snippet);
 }
